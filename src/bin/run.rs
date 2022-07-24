f4798972c3367796643b3eec4237cb0d16292134
Koichi Akabe <vbkaisetsu@gmail.com>
2022-07-24 18:31:49 +0900

    Update CLI

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -10,16 +10,21 @@ fn main() {
         let line = line.unwrap();
         sentences.push(Sentence::from_tokenized(&line).unwrap());
     }
+    eprintln!("Initializing...");
     let mut aligner = Aligner::new(&sentences, 1);
-    for i in 0..10 {
-        let diff = aligner.update();
-        eprintln!("iter {i}: log_diff = {diff}");
+    eprintln!("Training...");
+    for i in 0..20 {
+        let log_diff = aligner.update();
+        eprintln!("#{i} log_diff: {log_diff}");
+        if log_diff < -20.0 {
+            break;
+        }
     }
     eprintln!("Finalizing...");
-    let model = aligner.finalize();
+    let phoneme_map = aligner.finalize();
     let mut buf = String::new();
     for mut sentence in sentences {
-        model.make_alignment(&mut sentence, 1);
+        phoneme_map.make_alignment(&mut sentence, 1);
         sentence.write_tokenized_text(&mut buf);
         println!("{}", buf);
     }
